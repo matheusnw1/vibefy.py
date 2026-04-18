3f1f7def16e54808e0195ee070015dc040dd3652
Matheus Navarro <matheus.wenceslau@eaportal.org>
2026-04-18 18:11:48 -0300

    integração com IA via OpenRouter funcionando

--- vibefy.py
+++ vibefy.py
@@ -1,5 +1,16 @@
 
 import requests
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+chave = os.getenv('OPENROUTER_API_KEY')
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=chave
+)
+
 
 def buscar_musicas(artista):
     lista = []
@@ -11,19 +22,31 @@ def buscar_musicas(artista):
         lista.append(music)
     return lista
 
+
 def exibir_musicas(musicas):
     i = 0
     for musica in musicas:
         i += 1
         print(f'Musica {i}: {musica}')
 
-def pedir_artista():
-    artista = input('Como você está se sentindo?: ')
+
+def interpretar_humor(humor):
+    resposta = client.chat.completions.create(
+        model="arcee-ai/trinity-large-preview:free",
+        messages=[
+            {"role": "user", "content": f'O usuário está se sentindo assim: {humor}. Me retorne apenas o nome de um artista musical que combina com esse sentimento, sem explicação, só o nome.'}
+        ]
+    )
+    artista = resposta.choices[0].message.content
     return artista
 
-artista = pedir_artista()
+
+def pedir_humor():
+    humor = input('Como você está se sentindo?: ')
+    return humor
+
+
+humor = pedir_humor()
+artista = interpretar_humor(humor)
 musicas = buscar_musicas(artista)
 exibir_musicas(musicas)
-
-
-
