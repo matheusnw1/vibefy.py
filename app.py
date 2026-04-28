66df0a9aa06eb70223234686df87dbd26ecf040e
Matheus Navarro <matheus.wenceslau@eaportal.org>
2026-04-28 17:51:16 -0300

    desenvolvimento da rota de busca no flask

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
     return render_template('index.html')
+
+@app.route('/buscar', methods=['POST'])
+def buscar():
+    dados = requests.json
+    return jsonify({'resultado': 'ok'})
 
 app.run()
